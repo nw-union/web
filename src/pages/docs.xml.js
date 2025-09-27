29cd92afb7ea6b72eb347a7fc0e232abe875e942
Taiki Hayashi <grandcolline@gmail.com>
2025-09-27 21:32:01 +0900

    feat: docs の feed 追加

---
+++ src/pages/docs.xml.js
@@ -0,0 +1,27 @@
+import rss from '@astrojs/rss';
+
+export async function GET(context) {
+  const allDocs = Object.values(import.meta.glob("./docs/*.md", { eager: true }));
+
+  // ドキュメントを新しい順に並び替えて、hiddenListがtrueのものを除外
+  const sortedDocs = allDocs
+    .sort((a, b) => {
+      const aDate = new Date(a.frontmatter.pubDate);
+      const bDate = new Date(b.frontmatter.pubDate);
+      return bDate.getTime() - aDate.getTime();
+    })
+    .filter((doc) => !doc.frontmatter.hiddenList);
+
+  return rss({
+    title: 'Docs | NWU',
+    description: 'ドキュメント一覧',
+    site: `${context.site}/docs`,
+    items: sortedDocs.map((doc) => ({
+      title: doc.frontmatter.title,
+      pubDate: doc.frontmatter.pubDate,
+      description: doc.frontmatter.description,
+      customData: doc.frontmatter.customData,
+      link: `${doc.url}`,
+    })),
+  });
+}
