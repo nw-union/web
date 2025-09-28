4d2b49a5f2574f3b459d76fe3b6b21fbb9f7c315
Taiki Hayashi <grandcolline@gmail.com>
2025-09-28 20:20:29 +0900

    feature: tailwind v4 アップグレード対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,19 @@
 // @ts-check
+import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nw-union.net",
   outDir: "./build",
-  server: {
-    host: "0.0.0.0",
-    port: 4321,
+  vite: {
+    plugins: [tailwindcss()],
   },
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
-
     imageService: "cloudflare",
   }),
-  integrations: [tailwind()],
 });
