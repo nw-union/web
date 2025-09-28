8ed051006ccab7488502f7633607c008cea369f5
Taiki Hayashi <grandcolline@gmail.com>
2025-09-28 20:22:15 +0900

    fix: biome

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
