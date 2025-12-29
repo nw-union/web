0baf49f1cfc51ea53b4d4db5ebd292fb4cc0b697
Taiki Hayashi <grandcolline@gmail.com>
2025-12-29 19:32:25 +0900

    refactor

--- type.ts
+++ type.ts
@@ -1,6 +1,6 @@
 import type { AppError } from "@nw-union/nw-utils";
 import { newType } from "@nw-union/nw-utils/lib/zod";
-import { okAsync, type ResultAsync } from "neverthrow";
+import type { ResultAsync } from "neverthrow";
 import z from "zod";
 
 // ---------------------------
@@ -146,33 +146,6 @@ export interface KiokuWorkFlows {
   // キオクを探す
   get(): ResultAsync<Kioku[], AppError>;
 }
-
-export const newMockKiokuWorkFlows = (): KiokuWorkFlows => ({
-  get: () =>
-    okAsync([
-      {
-        id: "1",
-        title: "画像について",
-        name: "grandcolline",
-        category: "doc",
-        thumbnailUrl: "https://nw-union.net/img/logo card.png",
-        url: "https://example.com/doc/1",
-        createdAt: new Date(),
-      },
-      {
-        id: "2",
-        title:
-          "【日記】2025.10.26 OASIS を観た！【hidelberq / 抑止力 / hisanori ito / 🐙】",
-        name: "NWU",
-        category: "privateYoutube",
-        thumbnailUrl:
-          "https://img.youtube.com/vi/3dplZ0KaxdE/maxresdefault.jpg",
-        duration: "10:00",
-        url: "https://youtube.com/watch?v=sample",
-        createdAt: new Date(),
-      },
-    ]),
-});
 
 // ---------------------------
 
